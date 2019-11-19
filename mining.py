17759e1baad6e7ec9c71c6f8e9291b53a7f6f62f
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-19 18:10:49 +0100

    mining function

---
+++ mining.py
@@ -0,0 +1,67 @@
+import time
+import json
+import hashlib
+from time import time
+from uuid import uuid4
+
+import sys
+
+class Lambda_Coin(object):
+	def __init__(self):
+		self.chain = []
+		self.current_transactions = []
+
+		self.new_block(previous_hash=1, proof=100)
+
+	def new_block(self, proof, previous_hash=None):
+		"""
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+		block = {
+			'index': len(self.chain) + 1,
+			'timestamp': time(),
+			'transactions': self.current_transactions,
+			'proof': proof
+			'previous_hash': previous_hash or self.hash(self.chain[-1])
+		}
+
+		self.current_transactions = []
+		self.chain.append(block)
+
+		return block
+
+	@staticmethod
+	def hash(self, block):
+		"""
+        Creates a SHA-256 hash of a Block
+        :param block": <dict> Block
+        "return": <str>
+        """
+
+		string_object = json.dumps(block, sort_keys=True)
+		block_string = string_object.encode()
+		raw_hash = hashlib.sha256(block_string)
+		hex_hash = raw_hash.hexdigest()
+		return hex_hash
+
+	@property
+	def last_block(self):
+		return self.chain[-1]
+
+	def proof_of_work(self, block):
+		pass
+
+
+
+blockchain = Lambda_Coin()
+def full_chain():
+		response = {
+			'length': len(blockchain.chain),
+			'chain': blockchain.chain
+		}
+		return jsonify
+
